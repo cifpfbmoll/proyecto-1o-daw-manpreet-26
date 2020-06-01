f2b2c7095f51ec419dec3a39125cf0d7e1cc617b
luban <luban@LAPTOP-J73O5PHV>
2020-06-02 01:16:41 +0200

    Merge origin/master
    Conflicts:
    	src/javaproyecto/iniciar_sesion.java
    he creado conexion en clase main
    he creado clase para mostrar lista de los empleados
    termino clase para actualitzar empleados
    he creado clase asistencia sin terminar
    trabajando con la clase de asistencia
    he conectado clase de actualitzar empleados y lista de empleados con clase main
    he hecho cambio en cllase asistencia y creado clase para actualitzar empleados salario
    he creado lista de asisttencia
    he creado la clase generarNomina para generar Nomina
    hecho cambios en clase main del proyecto
    he hecho comentarios  para javadoc
    insertado los datos de empleado
    hecho clase empleado en nuevo manera
    he creado menu principal in clase main del proyecto
    termino clase salarioEmpleado con metodo de agregar y actualizar

--- src/javaproyecto/Departamentos.java
+++ src/javaproyecto/Departamentos.java
@@ -5,14 +5,55 @@
  */
 package javaproyecto;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 /**
  *
  * @author luban
  */
-class Departamentos {
+public class Departamentos {
+    private int id;
+    private String nombreDepartamento;
+    
+    Scanner lector = new Scanner(System.in);
 
-    void agregarDepartamento() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Departamentos() {
+    }
+
+    public Departamentos(int id, String nombreDepartamento) {
+        this.id = id;
+        this.nombreDepartamento = nombreDepartamento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreDepartamento() {
+        return nombreDepartamento;
+    }
+
+    public void setNombreDepartamento(String nombreDepartamento) {
+        this.nombreDepartamento = nombreDepartamento;
     }
     
+    public void agregarDepartamento() throws SQLException{
+        System.out.println("El id del departamento es : ");
+        this.setId(lector.nextInt());
+        System.out.println("El nombre del departamento es : ");
+        this.setNombreDepartamento(lector.next());
+        
+        String query = "insert into departamento (depId,depNombre) values (?,?)";
+        PreparedStatement pst = conecion_bbdd.establecerConexion().prepareStatement(query);
+        
+        pst.setInt(1, id);
+        pst.setString(2, nombreDepartamento);
+        pst.executeUpdate();
+    }
 }
--- src/javaproyecto/salarioEmpleado.java
+++ src/javaproyecto/salarioEmpleado.java
@@ -5,22 +5,132 @@
  */
 package javaproyecto;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 /**
  *
  * @author luban
  */
-class salarioEmpleado {
+public class salarioEmpleado extends Empleado {
 
-    void agregarEmpleado() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private float salarioBase;
+    private float irpf;
+    private float baseDeCotitzacion;
+    private float salarioNet;
+
+    Scanner input = new Scanner(System.in);
+
+    public salarioEmpleado() {
     }
 
-    void actualizarEmpleado() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public salarioEmpleado(float salarioBase, float irpf, float baseDeCotitzacion, float salarioNet) {
+        this.salarioBase = salarioBase;
+        this.irpf = irpf;
+        this.baseDeCotitzacion = baseDeCotitzacion;
+        this.salarioNet = salarioNet;
     }
 
-    void generarNomina() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public salarioEmpleado(int id, String nombre, String apellido1, String apellido2, String correo, int movil, float salarioNet, int depId) {
+        super(id, nombre, apellido1, apellido2, correo, movil, salarioNet, depId);
+    }
+
+    public float getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(float salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public float getIrpf() {
+        return irpf;
+    }
+
+    public void setIrpf(float irpf) {
+        this.irpf = irpf;
+    }
+
+    public float getBaseDeCotitzacion() {
+        return baseDeCotitzacion;
+    }
+
+    public void setBaseDeCotitzacion(float baseDeCotitzacion) {
+        this.baseDeCotitzacion = baseDeCotitzacion;
+    }
+
+    public float getSalarioNet() {
+        return salarioNet;
+    }
+
+    public void setSalarioNet(float salarioNet) {
+        this.salarioNet = salarioNet;
+    }
+
+    public void calculaSalarioNeto() {
+        salarioNet = ((salarioBase + baseDeCotitzacion) - irpf);
+        System.out.println("El salario net es : " + salarioNet);
+    }
+
+    public void agregarEmpleado() throws SQLException {
+        System.out.println("El salario basico del empleado es : ");
+        this.setSalarioBase(input.nextFloat());
+        System.out.println("El IRPF del empleado es :");
+        this.setIrpf(input.nextFloat());
+        System.out.println("La base de cotitzación del empleado es :");
+        this.setBaseDeCotitzacion(input.nextFloat());
+        this.calculaSalarioNeto();
+
+        String query = "insert into salario(empId,salarioBase,irpf,baseDeCotitzacion,salarioNeto) values (null,?,?,?,?)";
+
+        PreparedStatement pst = conecion_bbdd.establecerConexion().prepareStatement(query);
+
+        pst.setFloat(1, salarioBase);
+        pst.setFloat(2, irpf);
+        pst.setFloat(3, baseDeCotitzacion);
+        pst.setFloat(4, salarioNet);
+
+        pst.executeUpdate();
+    }
+
+    public void actualizarEmpleado() throws SQLException {
+        System.out.println("El salario basico del empleado es : ");
+        this.setSalarioBase(input.nextFloat());
+        System.out.println("El IRPF del empleado es :");
+        this.setIrpf(input.nextFloat());
+        System.out.println("La base de cotitzación del empleado es :");
+        this.setBaseDeCotitzacion(input.nextFloat());
+        this.calculaSalarioNeto();
+        System.out.println("El id del empleado es : ");
+        this.setId(input.nextInt());
+        
+        String q = "update salario set salarioBase=?,irpf=?,baseDeCotitzacion=?,salarioNeto=? where empId=?";
+        PreparedStatement ps = conecion_bbdd.establecerConexion().prepareStatement(q);
+        
+        ps.setFloat(1, salarioBase);
+        ps.setFloat(2, irpf);
+        ps.setFloat(3, baseDeCotitzacion);
+        ps.setFloat(4, salarioNet);
+        ps.setInt(5, this.getId());
+        ps.executeUpdate();
+        
     }
     
+    public void generarNomina() throws SQLException{
+        System.out.println("El id del empleado es : ");
+        this.setId(input.nextInt());
+        
+        String qu = "select * from salario where empId =?";
+        PreparedStatement ps = conecion_bbdd.establecerConexion().prepareStatement(qu);
+        ps.setInt(1, this.getId());
+        ResultSet rs = ps.executeQuery();
+        System.out.println("Salario base :" + rs.getFloat((int) salarioBase));
+        System.out.println("irpf :" + this.getIrpf());
+        System.out.println("base de cotitzacion :" + this.getBaseDeCotitzacion());
+        System.out.println("salario neto :" + this.getSalarioNet());
+        
+    }
+
 }
