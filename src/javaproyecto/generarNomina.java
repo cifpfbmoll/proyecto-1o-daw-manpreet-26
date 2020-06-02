1877bbb3d342a333ee02d2c9e21ee64f94e01d49
luban <luban@LAPTOP-J73O5PHV>
2020-06-02 15:07:40 +0200

    Merge origin/master
    Conflicts:
    	src/javaproyecto/iniciar_sesion.java
    he creado conexion en clase main
    he creado clase para mostrar lista de los empleados
    termino clase para actualitzar empleados
    he creado clase asistencia sin terminar
    trabajando con la clase de asistencia
    he conectado clase de actualitzar empleados y lista de empleados con clase main
    he hecho cambio en cllase asistencia y creado clase para actualitzar empleados salario
    he creado lista de asisttencia
    he creado la clase generarNomina para generar Nomina
    hecho cambios en clase main del proyecto
    he hecho comentarios  para javadoc
    insertado los datos de empleado
    hecho clase empleado en nuevo manera
    he creado menu principal in clase main del proyecto
    termino clase salarioEmpleado con metodo de agregar y actualizar
    acabo clase departamentos con metodo de agregar y actualizar
    he creado metodo para mostrar datos de empleados
    agrego nuevo campos en menu
    agrego metodos en clase empleado
    generado nomina

--- src/javaproyecto/generarNomina.java
+++ src/javaproyecto/generarNomina.java
@@ -3,61 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/*package javaproyecto;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-/**
- *
- * @author luban
- *//*
-public class generarNomina extends Empleado {
-
-    public void generaNomina() throws SQLException, IOException {
-
-        String query = "select empId,nombre,apellido1,apellido2,correo,movil from empleado";
-        PreparedStatement ps = conecion_bbdd.establecerConexion().prepareStatement(query);
-
-        ResultSet rs = ps.executeQuery();
-        FileWriter fstream = new FileWriter("file.txt");
-        try (BufferedWriter out = new BufferedWriter(fstream)) {
-            while (rs.next()) {
-                
-                out.write(Integer.toString(rs.getInt("id")) + ", ");
-                out.write(rs.getString("nombre") + ", ");
-                out.write(rs.getString("apellido1") + ", ");
-                out.write(rs.getString("apellido2") + ", ");
-                out.write(rs.getString("correo") + ", ");
-                out.write(Integer.toString(rs.getInt("movil")) + ", ");
-                out.newLine();
-                
-            }
-            System.out.println("hecho escribir dato en archivo");
-        }catch(Exception e){
-            e.getMessage();
-        }
-        
-        
-    }
-    
-}*//*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaproyecto;
 
-import java.io.BufferedWriter;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 /**
@@ -65,10 +19,14 @@ import java.sql.SQLException;
  * @author luban
  */
 public class generarNomina extends Empleado {
+
+    private static void mostrarTablas(DatabaseMetaData dbmd) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
     Empleado emp = new Empleado();
 
-    /*public void generaNomina() throws SQLException, IOException {
+    /*public void generaNomina1() throws SQLException, IOException {
 
         try {
             String query = "select empId,nombre from empleado";
@@ -90,20 +48,20 @@ public class generarNomina extends Empleado {
             FileWriter fstream = new FileWriter("file.txt");
             BufferedWriter out = new BufferedWriter(fstream);
 
-            while (rs.next()) {*/
+            while (rs.next()) {
 
-                /*out.write(Integer.toString(rs.getInt("empId")) + ", ");
-            out.write(Integer.toString(rs.getInt("depID")) + ", ");
-            out.write(rs.getString("nombre") + ", ");
-            out.write(rs.getString("apellido1") + ", ");
-            out.write(rs.getString("apellido2") + ", ");
-            out.write(rs.getString("correo") + ", ");
-            out.write(Integer.toString(rs.getInt("movil")) + ", ");
-            out.newLine();
-            
-            System.out.println("hecho escribir dato en archivo");
-            out.close();*/
-               /* try {
+                out.write(Integer.toString(rs.getInt("empId")) + ", ");
+                out.write(Integer.toString(rs.getInt("depID")) + ", ");
+                out.write(rs.getString("nombre") + ", ");
+                out.write(rs.getString("apellido1") + ", ");
+                out.write(rs.getString("apellido2") + ", ");
+                out.write(rs.getString("correo") + ", ");
+                out.write(Integer.toString(rs.getInt("movil")) + ", ");
+                out.newLine();
+
+                System.out.println("hecho escribir dato en archivo");
+                out.close();
+                try {
                     int empId = rs.getInt("empId");
                     String nombre = rs.getString("nombre");
 
@@ -120,18 +78,88 @@ public class generarNomina extends Empleado {
         } catch (IOException | SQLException e1) {
             e1.getMessage();
         }
-    }*/
-    
-    public void generaNomina() throws SQLException {
-        String query = "select * from empleado";
-        PreparedStatement ps = conecion_bbdd.establecerConexion().prepareStatement(query);
-        
-        ResultSet rs = ps.executeQuery();
-        if(rs.next()){
-            System.out.println("ya tienes ");
-        }else{
-            System.out.println("tiene erro");
-        }
     }
-}
+
+    /*public void generaNomina() throws IOException, SQLException {
+        CSVWriter writer = new CSVWriter(new FileWriter("yourfile.csv"), '\t');
+        Boolean includeHeaders = true;
+
+        String query = "select empId,nombre from empleado";
+            PreparedStatement pst = conecion_bbdd.establecerConexion().prepareStatement(query);
+
+        java.sql.ResultSet myResultSet = pst.executeQuery();//your resultset logic here
+
+writer.writeAll(myResultSet, includeHeaders);
+
+        writer.close();
+
+    }*/
+
+ /*private List<String> resultSetArray = new ArrayList<>();
+    public void generaNomina() throws Exception{
+        Connection connection = null;
+        fetchDataFromDatabase("SQL queries", connection);
+        printToCsv(resultArray);
+    }
+    public void fetchDataFromDatabase(String selectQuery,Connection connection) throws  Exception{
+            try {
+
+                String q ="select * from empleado";
+                PreparedStatement ps = conecion_bbdd.establecerConexion().prepareStatement(q);
+                ResultSet rs = ps.executeQuery();
+                int numCols = rs.getMetaData().getColumnCount();
+
+                while(rs.next()) {
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int i = 1; i <= numCols; i++) {
+                        sb.append(String.format(String.valueOf(rs.getString(i))) + " ");
+
+                    }
+                    resultSetArray.add(sb.toString());
+
+                }
+
+            } catch (SQLException e) {
+                LOGGER.error("Sql exception " + e.getMessage());
+            }
+
+        }
+    public static void printToCsv(List<String> resultArray) throws Exception{
+
+        File csvOutputFile = new File("file_name");
+        FileWriter fileWriter = new FileWriter(csvOutputFile, false);
+
+
+        for(String mapping : resultArray) {
+            fileWriter.write(mapping + "\n");
+         }
+
+        fileWriter.close();
+
+    }*/
+    public void generaNomina() throws SQLException {
+
+        try {
+            String q = "select empId,nombre,apellido1,apellido2,correo,movil,depId from empleado";
+            PreparedStatement psmnt = conecion_bbdd.establecerConexion().prepareStatement(q);
+            ResultSet rs = psmnt.executeQuery();
+
+            while (rs.next()) {
+                System.out.println("empID : " + rs.getInt(1));
+                System.out.println("nombre : " + rs.getString(2));
+                System.out.println("apellido1 : " + rs.getString(3));
+                System.out.println("apellido2 : " + rs.getString(4));
+                System.out.println("correo : " + rs.getString(5));
+                System.out.println("movil : " + rs.getInt(6));
+                System.out.println("depId : " + rs.getInt(7));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        }
+
+    }
+    
+     
 
